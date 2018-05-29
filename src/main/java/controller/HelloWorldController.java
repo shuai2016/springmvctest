0b2205d2883a576bbfdab1f63a1ba0f706b43c32
shuai <1627392348@qq.com>
2018-05-29 18:20:57 +0800

    开始shiro

--- src/main/java/controller/HelloWorldController.java
+++ src/main/java/controller/HelloWorldController.java
@@ -1,8 +1,15 @@
 package controller;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 @Controller
 public class HelloWorldController {
@@ -14,5 +21,11 @@ public class HelloWorldController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello World!!!";
+	}
+
+	@RequestMapping("shiro01")
+	public void shiro01(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.write("<h1>Hello World !</h1>");
 	}
 }
