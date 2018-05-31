533c0effaa227a6e0b03534131e7418e7592a39b
shuai <1627392348@qq.com>
2018-05-31 14:47:45 +0800

    使用spring,在配置文件中配置指定路径需要的角色权限信息，没有使用freemarker

--- src/main/java/controller/ShiroController.java
+++ src/main/java/controller/ShiroController.java
@@ -1,21 +1,56 @@
 package controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @Controller
 @RequestMapping("shiro")
 public class ShiroController {
-	@ResponseBody
-	@RequestMapping(value = "note",produces = "application/json; charset=utf-8")
-	public String note(){
-		return "请登录";
-	}
 
 	@RequestMapping("login")
-	public String login(){
-		return "login";
+	public void login(HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("请登陆");
 	}
 
+	@RequestMapping("validate")
+	public void validate(HttpServletResponse response,String username, String password) throws IOException {
+		//登陆
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+		subject.login(token);
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("已登陆");
+	}
+
+	@RequestMapping("addRole")
+	public void addRole(HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("添加角色");
+	}
+
+	@RequestMapping("deleteRole")
+	public void deleteRole(HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("删除角色");
+	}
+
+	@RequestMapping("message")
+	public void message(HttpServletResponse response) throws IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("您没有相应的权限");
+	}
 }
