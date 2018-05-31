b2fd14b52eb72cbd58c254574b022d2de8fddecb
shuai <1627392348@qq.com>
2018-05-31 16:13:25 +0800

    使用spring,使用自定义类指定路径需要的角色权限信息，没有使用freemarker

--- src/main/java/shiro/ChainDefinitionSectionMetaSource.java
+++ src/main/java/shiro/ChainDefinitionSectionMetaSource.java
@@ -14,7 +14,7 @@ public class ChainDefinitionSectionMetaSource implements FactoryBean<Ini.Section
 		//加载默认的url
 		ini.load(filterChainDefinitions);
 		Ini.Section section = ini.getSection(Ini.DEFAULT_SECTION_NAME);
-		//循环Resource的url,逐个添加到section中。section就是filterChainDefinitionMap,
+		//循环Resource的url,逐个添加到section中。section就是filterChainDefinitionMap
 		//里面的键就是链接URL,值就是存在什么条件才能访问该链接
 		section.put("/shiro/addPermission", "perms[add3]");
 		section.put("/shiro/deletePermission", "perms[delete2]");
