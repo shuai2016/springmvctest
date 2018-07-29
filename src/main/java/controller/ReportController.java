0db1ee1230e630af36485cdc5380a351a2026692
shuai2016 <1627392348@qq.com>
2018-07-29 19:49:20 +0800

    修改修改修改

--- src/main/java/controller/ReportController.java
+++ src/main/java/controller/ReportController.java
@@ -1,10 +1,11 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import vo.Report;
+
+import java.util.List;
 
 @Controller
 public class ReportController {
@@ -16,8 +17,8 @@ public class ReportController {
 
     @ResponseBody
     @RequestMapping("addReportDo")
-    public String addReportDo(@RequestBody Report report){
-        System.out.println(report);
+    public String addReportDo(@RequestParam("array[]") List<String> list){
+        System.out.println(list);
         return "ok";
     }
 }
